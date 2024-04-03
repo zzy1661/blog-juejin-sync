9f2b1d0974e2169ced36d02afc651545176822ea
zzy1661 <1556597051@qq.com>
2024-04-03 16:28:32 +0800

    feat: 更新脚本

--- index.js
+++ index.js
@@ -1,118 +1,123 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const babelParser = require("@babel/parser");
-var babel = require("@babel/core");
-let acorn = require("acorn");
-
+const ids = require("./ids");
+// const ids = ['6996918972251635749']
+const puppeteer = require('puppeteer');
+const TurndownService = require('turndown');
 var fs = require("fs-extra");
-let ids = [
-    "7054572468400291853",
-    "7044826282302898184",
-    // "7043440914244567054",
-    // "7038908706024620045",
-    // "7038459812601921550",
-    // "7037478950733348901",
-    // "7037473968734863391",
-    // "7036354289215733791",
-    // "7035543139179823118",
-    // "7035216237357432840",
-    // "7034857481876537380",
-    // "7034526305743798285",
-    // "7034007302621364261",
-    // "7033640446907645983",
-    // "7033306469378293791",
-    // "7032940757937815583",
-    // "7032663396160012295",
-    // "7032289105358618632",
-    // "7031719940348624933",
-    // "7031508533820686350",
-    // "7031189517776191518",
-    // "7030786939619901470",
-    // "7030454568731443208",
-    // "7030003426209726472",
-    // "7029578488881348621",
-    // "7029328849338892319",
-    // "7028842630758793246",
-    // "7028468626818760711",
-    // "7028165657954910222",
-    // "7027807945903570957",
-    // "7027372909135921183",
-    // "7026928507930279966",
-    // "7026630608486924295",
-    // "7026207350239264781",
-    // "7025925719330914340",
-    // "7025503828157939719",
-    // "7018852084803960868",
-    // "7018765637870698503",
-    // "7016889543823458334",
-    // "7016656649695789064",
-    // "7009115194374750222",
-    // "6998043554366881805",
-    // "6997590944346275876",
-    // "6996971685517721608",
-    // "6996918972251635749",
-    // "6990271352460541982",
-    // "6966609674023993351",
-    // "6961324555319115807",
-    // "6959873199005761549",
-    // "6952422478132019237",
-    // "6916052183729307655",
-    // "6844904049305714702",
-    // "6844903971664953357",
-    // "6844904035665838087",
-    // "6844904035665838094",
-    // "6844904035661643790",
-    // "6844904035661643784",
-    // "6844903971664953351",
-    // "6844904035657449480",
-    // "6844904035657449485",
-    // "6844904035653255182",
-    // "6844904035649224717",
-    // "6844903934109155336",
-];
-function syncBlob(index) {
-    if(index>=ids.length){
-        console.log('全部完成')
-        return;
-    }
-    let id = ids[index];
-    let url = "https://juejin.cn/post/" + id;
-    
-    axios.get(url).then((res) => {
-        const $ = cheerio.load(res.data);
-        let str = $("body script")[0].children[0].data;
-        // fs.writeFile('./t.js',str)
-        try {
-            let ast = acorn.parse(str, { ecmaVersion: 2020 });
-            if (ast) {
-                let ps =
-                    ast.body[0].expression.right.callee.body.body[0].argument.properties;
-                let state = ps.find((item) => item.key.name == "state");
-                let view = state.value.properties.find(item => item.key.name == 'view')
-                let column = view.value.properties.find(item => item.key.name == 'column')
-                let entry = column.value.properties.find(item => item.key.name == 'entry')
-                let article_info = entry.value.properties.find(item => item.key.name == 'article_info')
-                let mark_content = article_info.value.properties.find(item => item.key.name == 'mark_content')
-                let title = article_info.value.properties.find(item => item.key.name == 'title');
-                let t = title.value.value + '.md';
-                // 非法文件名
-                let filePath = './blogs/' + t.replace(/[\\\/\:\*\?\"<>]/g,' ');
-                
-                fs.writeFile(filePath, mark_content.value.value, (err) => {
-                    if (!err) {
-                        console.log(`${t}：done`)
-                        syncBlob(index+1)
-                    } else {
-                        console.log(err)
-                    }
-                })
-            } else {
-                console.log("parse error");
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    });
-    
-}
-syncBlob(0)
+const cheerio = require("cheerio");
+const ProgressBar = require('progress');
+const sharp = require('sharp');
+const path = require('path');
+const axios = require('axios');
+const bar = new ProgressBar(':bar :percent :etas', {
+  complete: '=',
+  incomplete: ' ',
+  width: 50,
+  total: ids.length
+});
+
+const turndownService = new TurndownService({ codeBlockStyle: 'fenced' });
+(async () => {
+  // 启动一个无头浏览器实例
+  const browser = await puppeteer.launch({defaultViewport:{width:1920,height:1080},headless:false,});
+
+  // 打开一个新页面
+  const page = await browser.newPage();
+
+  ids.forEach(async (id, i) => {
+    setTimeout(async () => {
+      // 导航到目标网页
+      await page.goto('https://juejin.cn/post/' + id);
+      try {
+       // 等待一些动态内容加载完成
+       await page.waitForSelector('#article-root',{timeout:100000});
+       let markdown = `---
+ title: {{ title }}
+ date: {{ date }}
+ tags: {{ tags }}
+ categories: {{ categories }}
+---\n\n`
+       // $('time').innerText
+       // $('.tag-list-container').innerText
+       await page.waitForSelector('.article-title', { timeout: 100000 });
+ 
+       const contentTitle = await page.evaluate(() => {
+         return document.querySelector('.article-title').innerText;
+       });
+       const contentDate = await page.evaluate(() => {
+         return document.querySelector('time').innerText;
+       });
+       const contentTags = await page.evaluate(() => {
+         return document.querySelector('.tag-list-container')?.innerText;
+       });
+       const contentCategories = await page.evaluate(() => {
+         return document.querySelector('.meta-box .first-column .title')?.innerText;
+       });
+       markdown = markdown.replace('{{ categories }}',contentCategories? '['+contentCategories.trim()+']':'');
+       markdown = markdown.replace('{{ title }}', `"${contentTitle}"`);
+       markdown = markdown.replace('{{ date }}', contentDate);
+       if (contentTags) {
+         markdown = markdown.replace('{{ tags }}', '[' + contentTags.replace(/\n/g, ',') + ']');
+       } else {
+         markdown = markdown.replace('{{ tags }}', '');
+       }
+       // markdown+='# '+contentTitle+'\n' ;
+       // 在页面上执行JavaScript代码
+       const contentBody = await page.evaluate(() => {
+         return document.querySelector('#article-root').innerHTML;
+       });
+ 
+       const $ = cheerio.load(contentBody);
+ 
+       $('style').remove();
+       $('.code-block-extension-header').remove()
+       $('img').each( (i, el) => {
+         let src = $(el).attr('src');
+         const saveDir = './imgs';
+         const name = src.match(/tos-cn-i-k3u1fbpfcp\/(\S+)~/)?.[1];
+         if(name){
+           $(el).attr('src',`../imgs/${name}.png`);
+           (async ()=>{
+             const imageName = `${name}.png`;
+             const savePath = path.join(saveDir, imageName);
+             const response = await axios.get(src, {
+               responseType: 'stream',
+             });
+             const writer = fs.createWriteStream(savePath);
+             response.data.pipe(writer);
+             new Promise((resolve, reject) => {
+               writer.on('finish', resolve);
+               writer.on('error', reject);
+             }).catch(e=>{
+               console.error('Error downloading image:', error);
+             });
+           })()
+         }
+       })
+       
+       markdown += turndownService.turndown($.html());
+       let filePath = './blogs/' + contentTitle.replace(/[\\\/\:\*\?\"<>]/g, ' ') + '.md';
+       markdown = markdown.replace(/https:\/\/link.juejin.cn\?target=(https?)%3A%2F%2F/g,(m,p1)=>{
+         return p1+'://'
+       })
+       fs.writeFile(filePath, markdown, (err) => {
+         if (!err) {
+           bar.tick(1);
+         } else {
+           console.log(err)
+         }
+       })
+       if(i==ids.length-1){
+         await browser.close();
+       }
+      } catch (error) {
+        console.log('error',id)
+        page.reload()
+        // throw error  
+      }
+   
+    }, i * (Math.random()*10000))
+  })
+
+ 
+})()
+
---
+++ old.js
@@ -0,0 +1,117 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const babelParser = require("@babel/parser");
+var babel = require("@babel/core");
+let acorn = require("acorn");
+
+var fs = require("fs-extra");
+let ids = [
+    "7054572468400291853",
+    "7044826282302898184",
+    "7043440914244567054",
+    "7038908706024620045",
+    "7038459812601921550",
+    "7037478950733348901",
+    "7037473968734863391",
+    "7036354289215733791",
+    "7035543139179823118",
+    "7035216237357432840",
+    "7034857481876537380",
+    "7034526305743798285",
+    "7034007302621364261",
+    "7033640446907645983",
+    "7033306469378293791",
+    "7032940757937815583",
+    "7032663396160012295",
+    "7032289105358618632",
+    "7031719940348624933",
+    "7031508533820686350",
+    "7031189517776191518",
+    "7030786939619901470",
+    "7030454568731443208",
+    "7030003426209726472",
+    "7029578488881348621",
+    "7029328849338892319",
+    "7028842630758793246",
+    "7028468626818760711",
+    "7028165657954910222",
+    "7027807945903570957",
+    "7027372909135921183",
+    "7026928507930279966",
+    "7026630608486924295",
+    "7026207350239264781",
+    "7025925719330914340",
+    "7025503828157939719",
+    "7018852084803960868",
+    "7018765637870698503",
+    "7016889543823458334",
+    "7016656649695789064",
+    "7009115194374750222",
+    "6998043554366881805",
+    "6997590944346275876",
+    "6996971685517721608",
+    "6996918972251635749",
+    "6990271352460541982",
+    "6966609674023993351",
+    "6961324555319115807",
+    "6959873199005761549",
+    "6952422478132019237",
+    "6916052183729307655",
+    "6844904049305714702",
+    "6844903971664953357",
+    "6844904035665838087",
+    "6844904035665838094",
+    "6844904035661643790",
+    "6844904035661643784",
+    "6844903971664953351",
+    "6844904035657449480",
+    "6844904035657449485",
+    "6844904035653255182",
+    "6844904035649224717",
+    "6844903934109155336",
+];
+function syncBlob(index=0) {
+    if(index>=ids.length){
+        console.log('全部完成')
+        return;
+    }
+    let id = ids[index];
+    let url = "https://juejin.cn/post/" + id;
+    
+    axios.get(url).then((res) => {
+        const $ = cheerio.load(res.data);
+        let str = $("body script")[0].children[0].data;
+        try {
+            let ast = acorn.parse(str, { ecmaVersion: 2020 });
+            if (ast) {
+                let ps =
+                    ast.body[0].expression.right.callee.body.body[0].argument.properties;
+                let state = ps.find((item) => item.key.name == "state");
+                let view = state.value.properties.find(item => item.key.name == 'view')
+                let column = view.value.properties.find(item => item.key.name == 'column')
+                let entry = column.value.properties.find(item => item.key.name == 'entry')
+                let article_info = entry.value.properties.find(item => item.key.name == 'article_info')
+                let mark_content = article_info.value.properties.find(item => item.key.name == 'mark_content')
+                let title = article_info.value.properties.find(item => item.key.name == 'title');
+                let t = title.value.value + '.md';
+                // 非法文件名
+                let filePath = './blogs/' + t.replace(/[\\\/\:\*\?\"<>]/g,' ');
+                
+                fs.writeFile(filePath, mark_content.value.value, (err) => {
+                    if (!err) {
+                        console.log(`${t}：done`)
+                        syncBlob(index+1)
+                    } else {
+                        console.log(err)
+                    }
+                })
+            } else {
+                console.log("parse error");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    });
+    
+}
+syncBlob()
